73e1c2aee1e1a23e63f5248be62afc49eec0812e
psangameswaran <priya.sangameswaran@gmail.com>
2017-06-14 16:47:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,58 +1,44 @@
-from flask import Flask, render_template, request, redirect
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-  return redirect('/index')
-
-@app.route('/index')
-def index():
-  return render_template('index.html')
-
-if __name__ == '__main__':
-  app.run(port=33507)
-
-import quandl
-quandl.ApiConfig.api_key = 'J3DvHz2kKB97tvnzutbs'
-data = quandl.get_table('WIKI/PRICES', date = { 'gte': '2017-05-15' })
-
-import simplejson as json
 import pandas as pd
-
-from ipywidgets import interact
 import numpy as np
+import bokeh.charts as bc
+from bokeh.resources import CDN
+from bokeh.embed import components
 
-from bokeh.io import push_notebook, show, output_notebook
-from bokeh.plotting import figure
-from bokeh.layouts import widgetbox
-from bokeh.models.widgets import TextInput
-output_notebook()
+@app.route("/")
+def visualisation():
+ # Build the dataframe
+ df = pd.DataFrame({
+ 'x': 2*np.pi*i/100,
+ 'sin': np.sin(2*np.pi*i/100),
+ 'cos': np.cos(2*np.pi*i/100),
+ } for i in range(0,101))
 
-data['date'] = data['date'].astype('datetime64[ns]')
-import datetime as dt     
-today = dt.date.today()
-monthago=today - pd.offsets.Day(29)
-data2=data[data['date']>monthago]
-x=data2['date'].dt.date
-a=data2['close']
-y=a[data2['ticker']=='A']
+ # Create the plot
+ plot = bc.Line(title='Triganometric fun!',
+ data=df, x='x', ylabel='y')
 
-p = figure(title="Stock Ticker Price", plot_height=300, plot_width=600)
-r = p.line(x, y, color="#2222aa", line_width=3)
-from bokeh.models import DatetimeTickFormatter
-p.xaxis.formatter=DatetimeTickFormatter(days=["%m/%d"])
+ # Generate the script and HTML for the plot
+ script, div = components(plot)
 
-def update(Ticker):
-    new_y=a[data2['ticker']==Ticker]
-    r.data_source.data['y']=new_y
-    push_notebook()
+ # Return the webpage
+ return """
+<!doctype html>
+<head>
+ <title>My wonderful trigonometric webpage</title>
+ {bokeh_css}
+</head>
+<body>
+ <h1>Everyone loves trig!
+ {div}
 
-show(p,notebook_handle=True)
+ {bokeh_js}
+ {script}
+</body>
+ """.format(script=script, div=div, bokeh_css=CDN.render_css(),
+ bokeh_js=CDN.render_js())
 
-interact(update,Ticker=[x for x in data2['ticker'].unique()])
-
-from bokeh.embed import components 
-
-script, div = components(p)
-return render_template('graph.html', script=script, div=div)
+if __name__ == "__main__":
+ app.run(host='0.0.0.0', port=80)
